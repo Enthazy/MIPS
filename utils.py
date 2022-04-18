0345b28f05fc737bd898901e9a3f8cd83146e927
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-04-18 18:31:30 +0100

    Modify files for numba implementation

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import numpy as np
-
+from numba import jit, njit
 
 def cal_contact():
     pass
 
+@njit
 def FJPotential(x, y, d, r6=1e-3, epsilon=1e-3, **kwargs):
     t = np.sqrt(x**2 + y**2)
     rx = x/t
