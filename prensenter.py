c506b748a9307b6fe15c77b895204fea66f56977
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-05-02 18:13:31 +0100

    finish numba cpu version jupyter notebook

--- prensenter.py
+++ prensenter.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import sys
 from time import time
 import numpy as np
+
 class Presenter:
     def __init__(self):
         pass
@@ -13,3 +15,33 @@ class Presenter:
         ax.axis('equal')
         # ax.set_axis_off()
         plt.show()
+def save(file_name, data):
+    """ Saves the model to a numpy file.
+    """
+    print("Writing to " + file_name)
+    np.savez_compressed(file_name, **data)
+
+
+def load(file_name):
+    """ Loads the model from numpy file.
+    """
+    print("Loading from " + file_name)
+    return dict(np.load(file_name))
+
+def display(posx, posy):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    # Now, we draw our points with a gradient of colors.
+    ax.scatter(np.remainder(posx[:len(posx)//2],1), np.remainder(posy[:len(posx)//2],1), linewidths=0.5,
+               marker='o', s=5, facecolors='none', edgecolors='teal', cmap=plt.cm.jet)
+    ax.scatter(np.remainder(posx[len(posx)//2:],1), np.remainder(posy[len(posx)//2:],1), linewidths=0.5,
+               marker='o', s=5, facecolors='none', edgecolors='coral', cmap=plt.cm.jet)
+
+    ax.axis('equal')
+    ax.grid()
+    plt.savefig("./results/view.png")
+
+if __name__=="__main__":
+    file_name = "./results/state4000.npz"
+    data = load(file_name)
+    display(data['px'],data['py'])
+    print(data['px'])
