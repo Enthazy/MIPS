6a2f4a2f0872ac5d5b66454cb34141c1619b01c0
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-07-02 22:06:20 +0100

    modify the data saved

--- v1.0/main_lite.py
+++ v1.0/main_lite.py
@@ -2,15 +2,16 @@ from src import *
 
 def main():
     # Hyper-parameters
-    epoch = int(3e3)
+    epoch = int(1e1)
+    savetime = int(1e3)     # iterations in each epoch
 
     N = 4900  # number of particles
-    M = 60
-    Lx = 120 # box size x
-    Ly = 120 # box size y
+    M = 60    # number of grids
+    Lx = 120  # box size x
+    Ly = 120  # box size y
     step = 5e-6
     Pe = 120 #Peclet number
-    is_save = False
+    is_save = True
     is_load = False
     savepoint=0
     np.random.seed(714)
@@ -40,32 +41,42 @@ def main():
         print("=========Load savepoint successfully=========")
 
     # Calculate physical quantities
-    folding_frac = round(cal_folding(N,Lx,Ly),3)
-    print("folding ratio is: ",folding_frac)
-    print("Pe value is: ",Pe)
+    folding_frac = round(cal_folding(N, Lx, Ly), 3)
+    print("folding ratio is: ", folding_frac)
+    print("Pe value is: ", Pe)
+    import os
+    os.makedirs("./results/"+str(Pe)+"_"+str(folding_frac), exist_ok=True)
+
 
     grid = grid_seperation(grid, qx, qy, M, Lx, Ly)
     # Run
 
     @njit()
     def body(grid, qx, qy, px, py, theta):
-        for _ in range(epoch):
+        for _ in range(savetime):
             set_seed(_)
             qx, qy, px, py, theta = run(step, grid, qx, qy, px, py, theta, Pe, N, M, Lx, Ly)
             if _%50==0:
                 grid = grid_seperation(grid, qx, qy, M, Lx, Ly)
         return qx, qy, px, py, theta
 
-    qx, qy, px, py, theta = body(grid, qx, qy, px, py, theta)
+    for _e in range(epoch):
+        qx, qy, px, py, theta = body(grid, qx, qy, px, py, theta)
 
-    if is_save:
-        data = {'qx': qx,
-                'qy': qy,
-                'px': px,
-                'py': py,
-                'theta': theta,
-                }
-        save("./results/"+str(Pe)+"_"+str(folding_frac)+".npz", data)
+        if is_save:
+            data = {'epoch': _e,
+                    'Pe': Pe,
+                    'PackFrac': folding_frac,
+                    'N': N,
+                    'L': Lx,
+
+                    'qx': qx,
+                    'qy': qy,
+                    'px': px,
+                    'py': py,
+                    'theta': theta,
+                    }
+            save("./results/"+str(Pe)+"_"+str(folding_frac)+"/"+str(_e)+".npz", data)
 
     return qx, qy
 
