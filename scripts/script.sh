f8e890414ecbc9423f54c7041d746c2af6560872
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-07-05 16:30:04 +0100

    add script.sh for single test run

---
+++ scripts/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N over_damped_single
+#PBS -m a
+#PBS -q standard
+#PBS -k o
+
+cd $HOME/MIPS/v1.0
+# number of output data
+# number of iteration in each data
+# number of particles
+# number of grids
+# simulation size
+# value of Peclet number
+python3 main.py 1000 1000 4900 20 80 120
