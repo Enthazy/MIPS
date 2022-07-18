864d6c35dc20a780e52379b4038ff14ea880479a
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-07-18 18:10:29 +0100

    add scripts for v2

---
+++ v2.0/scripts/experiments/f54p120w10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N under_single
+#PBS -m a
+#PBS -q parallel16
+#PBS -k oe
+
+cd $HOME/MIPS/v2.0
+# number of output data
+# number of iteration in each data
+# number of particles
+# number of grids
+# time step 1e-8
+# simulation size
+# value of Peclet number
+# value of W value 1e-3
+python3 main.py 1000 1000 10000 20 100 120 120 10
---
+++ v2.0/scripts/experiments/f54p80w1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N f54p80w1
+#PBS -m a
+#PBS -q parallel16
+#PBS -k oe
+
+cd $HOME/MIPS/v2.0
+# number of output data
+# number of iteration in each data
+# number of particles
+# number of grids
+# time step 1e-8
+# simulation size
+# value of Peclet number
+# value of W value 1e-3
+python3 main.py 1000 1000 10000 20 10 120 80 1
