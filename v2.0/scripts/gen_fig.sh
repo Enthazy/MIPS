04e89273e3749103266c76aa799f15ec1a0305c3
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-07-30 23:28:18 +0100

    update

--- v2.0/scripts/gen_fig.sh
+++ v2.0/scripts/gen_fig.sh
@@ -4,8 +4,9 @@
 #PBS -q standard
 #PBS -k oe
 
-def folderpath=$HOME/MIPS/v2.0/results/final_states
+folderpath=$HOME/MIPS/v2.0/results/F60P120W1000T100000
+savepath=$HOME/MIPS/v2.0/fig/F60P120W1000T100000/
 
-for file in folderpath/*; do
-  echo $file
+for file in $folderpath/*; do
+  python3 gen_fig.py $file $savepath
 done
