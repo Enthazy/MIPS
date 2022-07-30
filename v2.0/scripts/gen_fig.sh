ff493528d51fb8b41098ec4619efc52e28b8b672
Enthazy <stephenx.wang@mail.utoronto.ca>
2022-07-30 23:32:56 +0100

    update

--- v2.0/scripts/gen_fig.sh
+++ v2.0/scripts/gen_fig.sh
@@ -7,6 +7,8 @@
 folderpath=$HOME/MIPS/v2.0/results/F60P120W1000T100000
 savepath=$HOME/MIPS/v2.0/fig/F60P120W1000T100000/
 
+cd $HOME/MIPS/v2.0
+
 for file in $folderpath/*; do
   python3 gen_fig.py $file $savepath
 done
